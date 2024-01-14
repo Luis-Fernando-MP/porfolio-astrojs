4c0948d7130360ed6251a587ad1889ca2a9c6581
╰•JUJU•╯ <luigfmp@gmail.com>
2024-01-14 11:42:45 -0500

    feat: :sparkles: agregue los componentes header y el “balls”

---
+++ src/icons/code/index.ts
@@ -0,0 +1,55 @@
+import AstroJs from './astroJs.webp'
+import CSS from './css.webp'
+import Docker from './docker.webp'
+import Figma from './figma.webp'
+import Git from './git.webp'
+import GitHub from './github.webp'
+import GraphQL from './graphql.webp'
+import HTML from './html.webp'
+import Java from './java.webp'
+import JavaScript from './javascript.webp'
+import Linux from './linux.webp'
+import MySQL from './mysql.webp'
+import NextJs from './nextJs.webp'
+import NestJs from './nestJs.webp'
+import NodeJs from './nodeJs.webp'
+import PostgreSQL from './postgreSQL.webp'
+import Python from './python.webp'
+import React from './react.webp'
+import Redis from './redis.webp'
+import Sass from './sass.webp'
+import Scrum from './scrum.webp'
+import TypeORM from './typeorm.webp'
+import typescript from './typescript.webp'
+import vite from './vite.webp'
+import ViteTest from './viteTest.webp'
+import VSCode from './vscode.webp'
+
+export const codeIcons = {
+  AstroJs: { ...AstroJs, color: '#E33A9120' },
+  CSS: { ...CSS, color: '#285EED20' },
+  Docker: { ...Docker, color: '#099CEC20' },
+  Figma: { ...Figma, color: '#F041FF20' },
+  Git: { ...Git, color: '#F0503320' },
+  GitHub: { ...GitHub, color: '#34333620' },
+  GraphQL: { ...GraphQL, color: '#E535AB20' },
+  HTML: { ...HTML, color: '#E44D2620' },
+  Java: { ...Java, color: '#73A1FB20' },
+  JavaScript: { ...JavaScript, color: '#F0DB4F20' },
+  Linux: { ...Linux, color: '#F0BA1E20' },
+  MySQL: { ...MySQL, color: '#21748E20' },
+  NextJs: { ...NextJs, color: '#3C3C3C20' },
+  NestJs: { ...NestJs, color: '#E0234E20' },
+  NodeJs: { ...NodeJs, color: '#539E4320' },
+  PostgreSQL: { ...PostgreSQL, color: '#2D74AD20' },
+  Python: { ...Python, color: '#FFDF5B20' },
+  React: { ...React, color: '#5FD4F520' },
+  Redis: { ...Redis, color: '#C6302B20' },
+  Sass: { ...Sass, color: '#CD669A20' },
+  Scrum: { ...Scrum, color: '#1B849A20' },
+  TypeORM: { ...TypeORM, color: '#FE020020' },
+  typescript: { ...typescript, color: '#007ACC20' },
+  vite: { ...vite, color: '#BD34FE20' },
+  ViteTest: { ...ViteTest, color: '#729B1B20' },
+  VSCode: { ...VSCode, color: '#42ABF220' }
+}
