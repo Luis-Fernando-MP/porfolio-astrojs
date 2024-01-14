5716ff923400255acf276c19799a4fc095309a15
╰•JUJU•╯ <luigfmp@gmail.com>
2024-01-14 13:19:16 -0500

    fix: :lipstick: corrección de errores de diseño

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
     'no-multiple-empty-lines': 'off',
     'no-tabs': 'off',
     indent: ['error', 2],
