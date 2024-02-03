bad2c25f02854edf36e20ebf72dad7d57a6ff2af
juliju <luigfmp>
2024-02-03 01:45:48 -0500

    feat: :sparkles: componente about integrado

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Header from './header/Header.astro'
 import Projects from './projects/Projects.astro'
 import Project from './project/Project.astro'
 import Experience from './experience/Experience.astro'
+import AboutMe from './aboutMe/AboutMe.astro'
 
-export { Experiences, Header, Projects, Experience, Project }
+export { Experiences, Header, Projects, Experience, Project, AboutMe }
